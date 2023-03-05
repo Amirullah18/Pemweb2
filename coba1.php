59963c00cc86b46a2758a2c295c4825fa3c2fc52
Amirullah18 <114376832+Amirullah18@users.noreply.github.com>
2023-03-05 07:42:26 +0700

    hapus

--- coba1.php
+++ coba1.php
@@ -1,9 +1,1 @@
-<?php
-   define('PHI', 3.14);
-   define('DBNAME','inventori');
-   define('DBNSERVER','localhost');
 
-   echo 'PHI :'.PHI;
-   echo 'Nama database nya : '.DBNAME;
-   echo '<br>Lokasi database nya :' .DBNSERVER;
-?>
